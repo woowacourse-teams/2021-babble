0b28bd75e6a01b86d93e4592829ccf4dca9172a0
Min Kyung Kang <mnk918p@gmail.com>
2021-07-09 10:36:37 +0900

    프로젝트 설정(webpack, babel)을 진행한다. (#10)
    * chore: 프론트엔드 프로젝트 세팅
    
    * docs: 프로젝트 README 작성
    
    * chore: gitignore 세팅
    
    * docs: merge with initial commit
    
    Co-authored-by: unluckyjung <ybook2012@gmail.com>

---
+++ front/src/App.js
@@ -0,0 +1,9 @@
+import './index.scss';
+
+import React from 'react';
+
+const App = () => {
+  return <div className='app'>Babble</div>;
+};
+
+export default App;
---
+++ front/webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const path = require('path');
+
+module.exports = (env) => {
+  const entryPath =
+    env.mode === 'production' ? './dist/index.js' : './index.js';
+
+  return {
+    entry: entryPath,
+
+    output: {
+      path: path.join(__dirname, '/dist'),
+      publicPath: '/',
+      filename: 'bundle.js',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.(scss|sass)$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+
+    resolve: {
+      extensions: ['.js', '.jsx', 'scss'],
+    },
+
+    devServer: {
+      contentBase: path.join(__dirname, '/dist'),
+      publicPath: '/',
+      hot: true,
+      inline: true,
+      host: 'localhost',
+      port: 3000,
+      historyApiFallback: true,
+    },
+
+    devtool: 'inline-source-map',
+
+    plugins: [
+      new ErrorOverlayPlugin(),
+      new CleanWebpackPlugin(),
+      new ESLintPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+    ],
+
+    target: 'web',
+  };
+};
