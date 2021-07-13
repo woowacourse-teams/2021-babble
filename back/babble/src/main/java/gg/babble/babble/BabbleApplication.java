91a3c364b9a063b41bcde9b7f649fdf326099b0d
ROOT <junroot0909@gmail.com>
2021-07-13 20:42:47 +0900

    chore: spring 초기 세팅 (#22)

---
+++ back/babble/src/main/java/gg/babble/babble/BabbleApplication.java
@@ -0,0 +1,13 @@
+package gg.babble.babble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabbleApplication.class, args);
+	}
+
+}
