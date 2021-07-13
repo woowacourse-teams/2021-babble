ca8c1611b3dca0b73714cc87724bbb0cfbc154d4
ROOT <junroot0909@gmail.com>
2021-07-13 20:32:19 +0900

    chore: spring 초기 세팅

---
+++ back/babble/src/test/java/gg/babble/babble/BabbleApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.babble.babble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
