a53f81a6698dfaaf0e107ba3e0bf8834fa509278
lns13301 <rkdrbgks22@ks.ac.kr>
2021-07-14 16:18:47 +0900

    test: 게임 Id 가 없을 경우 예외 테스트 추가

---
+++ back/babble/src/main/java/gg/babble/babble/exception/BabbleNotFoundException.java
@@ -0,0 +1,12 @@
+package gg.babble.babble.exception;
+
+public class BabbleNotFoundException extends BabbleException {
+
+    public BabbleNotFoundException() {
+        super();
+    }
+
+    public BabbleNotFoundException(String message) {
+        super(message);
+    }
+}
--- back/babble/src/test/java/gg/babble/babble/domain/GameTest.java
+++ back/babble/src/test/java/gg/babble/babble/domain/GameTest.java
@@ -1,8 +1,11 @@
 package gg.babble.babble.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import gg.babble.babble.exception.BabbleNotFoundException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,9 @@ public class GameTest {
     @Autowired
     private GameRepository gameRepository;
 
+    @Autowired
+    private GameService gameService;
+
     @DisplayName("게임 더미 데이터를 확인한다.")
     @ParameterizedTest
     @CsvSource({"1, League Of Legend", "2, Overwatch", "3, Apex Legend"})
@@ -24,5 +30,12 @@ public class GameTest {
         Optional<Game> game = gameRepository.findById(id);
         assertThat(game.isPresent()).isTrue();
         assertThat(game.get().getName()).isEqualTo(gameName);
+    }
+
+    @DisplayName("게임 Id가 없을 경우 예외를 던진다.")
+    @Test
+    void gameNotFoundTest() {
+        assertThatThrownBy(() -> gameService.findById(Long.MAX_VALUE))
+                .isInstanceOf(BabbleNotFoundException.class);
     }
 }
