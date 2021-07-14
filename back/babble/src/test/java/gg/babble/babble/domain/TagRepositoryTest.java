d34a7f8f77397832d1c92b4237825a3d9b3fae17
ROOT <junroot0909@gmail.com>
2021-07-14 17:07:23 +0900

    test: 태그 더미 데이터 테스트

---
+++ back/babble/src/test/java/gg/babble/babble/domain/TagRepositoryTest.java
@@ -0,0 +1,22 @@
+package gg.babble.babble.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TagRepositoryTest {
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @DisplayName("태그 더미 데이터를 확인한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"실버", "2시간", "솔로랭크"})
+    void dummyGameTest(String tagName) {
+        assertThat(tagRepository.existsById(tagName)).isTrue();
+    }
+
+}
