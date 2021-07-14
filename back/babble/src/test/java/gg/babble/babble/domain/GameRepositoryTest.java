b6254239cc6d685f2b39d29b80e45e1f4258d4e5
lns13301 <rkdrbgks22@ks.ac.kr>
2021-07-14 16:39:16 +0900

    refactor: 게임 테스트 관련 리팩토링

--- back/babble/src/test/java/gg/babble/babble/domain/GameRepositoryTest.java
+++ back/babble/src/test/java/gg/babble/babble/domain/GameRepositoryTest.java
@@ -1,28 +1,20 @@
 package gg.babble.babble.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import gg.babble.babble.exception.BabbleNotFoundException;
-import gg.babble.babble.service.GameService;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-public class GameTest {
+public class GameRepositoryTest {
 
     @Autowired
     private GameRepository gameRepository;
-
-    @Autowired
-    private GameService gameService;
 
     @DisplayName("게임 더미 데이터를 확인한다.")
     @ParameterizedTest
@@ -31,12 +23,5 @@ public class GameTest {
         Optional<Game> game = gameRepository.findById(id);
         assertThat(game.isPresent()).isTrue();
         assertThat(game.get().getName()).isEqualTo(gameName);
-    }
-
-    @DisplayName("게임 Id가 없을 경우 예외를 던진다.")
-    @Test
-    void gameNotFoundTest() {
-        assertThatThrownBy(() -> gameService.findById(Long.MAX_VALUE))
-                .isInstanceOf(BabbleNotFoundException.class);
     }
 }
---
+++ back/babble/src/test/java/gg/babble/babble/service/GameServiceTest.java
@@ -0,0 +1,21 @@
+package gg.babble.babble.service;
+
+import gg.babble.babble.exception.BabbleNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class GameServiceTest {
+
+    @Autowired
+    private GameService gameService;
+
+    @DisplayName("게임 Id가 없을 경우 예외를 던진다.")
+    @Test
+    void gameNotFoundTest() {
+        assertThatThrownBy(() -> gameService.findById(Long.MAX_VALUE))
+                .isInstanceOf(BabbleNotFoundException.class);
+    }
+}
