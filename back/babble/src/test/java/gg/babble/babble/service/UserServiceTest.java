d831c981fe9e073322b0aece88c91ae1c9b2416d
lns13301 <rkdrbgks22@ks.ac.kr>
2021-07-14 16:52:56 +0900

    test: 유저 Id가 없을 경우 예외 처리 구현

---
+++ back/babble/src/test/java/gg/babble/babble/service/UserServiceTest.java
@@ -0,0 +1,21 @@
+package gg.babble.babble.service;
+
+import gg.babble.babble.exception.BabbleNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @DisplayName("유저 Id가 없을 경우 예외를 던진다.")
+    @Test
+    void gameNotFoundTest() {
+        assertThatThrownBy(() -> userService.findById(Long.MAX_VALUE))
+                .isInstanceOf(BabbleNotFoundException.class);
+    }
+}
