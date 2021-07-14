b5dce3a44dae16460afbc939395a883de739d5b4
unluckyjung <ybook2012@gmail.com>
2021-07-14 17:27:01 +0900

    test : 웹 소켓 connect 테스트
    200 ok 를 받은뒤, 101로 다시 포팅 한것으로 예상되어짐

---
+++ back/babble/src/test/java/gg/babble/babble/AcceptanceTest.java
@@ -0,0 +1,25 @@
+package gg.babble.babble;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void test11(){
+
+    }
+}
