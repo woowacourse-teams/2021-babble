72cb5a0b20a2b2ce047652fddc8dd381bf3b9d75
ROOT <junroot0909@gmail.com>
2021-07-14 16:41:48 +0900

    test: 유저 더미 데이터 테스트

---
+++ back/babble/src/test/java/gg/babble/babble/domain/UserRepositoryTest.java
@@ -0,0 +1,23 @@
+package gg.babble.babble.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("유저 더미 데이터를 확인한다.")
+    @ParameterizedTest
+    @CsvSource({"1, 루트", "2, 와일더", "3, 포비"})
+    void dummyGameTest(Long id, String name) {
+        Optional<User> user = userRepository.findById(id);
+        assertThat(user.isPresent()).isTrue();
+        assertThat(user.get().getName()).isEqualTo(name);
+    }
+}
