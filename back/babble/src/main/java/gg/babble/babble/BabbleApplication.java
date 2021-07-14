224358c2b9079769b58b20bc04ba568930dfac99
ROOT <junroot0909@gmail.com>
2021-07-14 14:44:16 +0900

    feat: 스켈레톤 코드 작성 (#28)

--- back/babble/src/main/java/gg/babble/babble/BabbleApplication.java
+++ back/babble/src/main/java/gg/babble/babble/BabbleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BabbleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BabbleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BabbleApplication.class, args);
+    }
 
 }
---
+++ back/babble/src/main/java/gg/babble/babble/controller/RoomController.java
@@ -0,0 +1,11 @@
+package gg.babble.babble.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/api/rooms")
+@RestController
+public class RoomController {
+
+
+}
