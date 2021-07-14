0124cadd5a2ffa7f066fd1093399b2fac488f998
ROOT <junroot0909@gmail.com>
2021-07-14 16:35:24 +0900

    feat: 게임 Id가 없을 경우 예외 처리 구현

--- back/babble/src/test/java/gg/babble/babble/domain/GameTest.java
+++ back/babble/src/test/java/gg/babble/babble/domain/GameTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import gg.babble.babble.exception.BabbleNotFoundException;
+import gg.babble.babble.service.GameService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
