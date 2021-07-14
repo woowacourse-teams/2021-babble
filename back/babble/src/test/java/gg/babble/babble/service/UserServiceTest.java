391acc03a7ad979b31e6e8ecd8d7a8b8d5c20e8d
ROOT <junroot0909@gmail.com>
2021-07-14 16:56:37 +0900

    feat: 유저 Id가 없을 경우 예외 처리 구현

--- back/babble/src/test/java/gg/babble/babble/service/UserServiceTest.java
+++ back/babble/src/test/java/gg/babble/babble/service/UserServiceTest.java
@@ -4,9 +4,11 @@ import gg.babble.babble.exception.BabbleNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@SpringBootTest
 public class UserServiceTest {
 
     @Autowired
