ec159ac211de4eb95cdf72aac6c009db5177b1d4
hyeon9mak <jinha3507@gmail.com>
2021-07-16 15:34:01 +0900

    feat: 유저와 방을 매핑하는 세션 구현

---
+++ back/babble/src/main/java/gg/babble/babble/service/SessionService.java
@@ -0,0 +1,46 @@
+package gg.babble.babble.service;
+
+import gg.babble.babble.domain.Room;
+import gg.babble.babble.domain.Session;
+import gg.babble.babble.domain.User;
+import gg.babble.babble.domain.repository.SessionRepository;
+import gg.babble.babble.exception.BabbleNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionService {
+
+    private final SessionRepository sessionRepository;
+
+    public SessionService(final SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
+
+    public void create(final Room room, final String sessionId, final User user) {
+        Session session = Session.builder()
+            .room(room)
+            .sessionId(sessionId)
+            .user(user)
+            .build();
+
+        sessionRepository.save(session);
+    }
+
+    public Room findRoomBySessionId(final String sessionId) {
+        Session session = sessionRepository.findSessionBySessionId(sessionId)
+            .orElseThrow(() -> new BabbleNotFoundException("존재하지 않는 세션 Id 입니다."));
+
+        return session.getRoom();
+    }
+
+    public User findUserBySessionId(final String sessionId) {
+        Session session = sessionRepository.findSessionBySessionId(sessionId)
+            .orElseThrow(() -> new BabbleNotFoundException("존재하지 않는 세션 Id 입니다."));
+
+        return session.getUser();
+    }
+
+    public void delete(final String sessionId) {
+        sessionRepository.deleteBySessionId(sessionId);
+    }
+}
