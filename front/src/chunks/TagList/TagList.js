8066c8a7c0555e4588ec91f1243b9eb4cb2ec46c
Min Kyung Kang <mnk918p@gmail.com>
2021-07-16 23:04:57 +0900

    refactor: TagList에 좌우 스크롤 이벤트 추가

--- front/src/chunks/TagList/TagList.js
+++ front/src/chunks/TagList/TagList.js
@@ -1,34 +1,31 @@
 import './TagList.scss';
 
+import React, { useRef } from 'react';
+
 import Caption1 from '../../core/Typography/Caption1';
-import { IoCloseOutline } from 'react-icons/io5';
-import { IoRemove } from 'react-icons/io5';
 import LinearLayout from '../../core/Layout/LinearLayout';
 import PropTypes from 'prop-types';
-import React from 'react';
-import Subtitle3 from '../../core/Typography/Subtitle3';
 import Tag from '../../components/Tag/Tag';
 
 const TagList = ({ tags }) => {
+  const tagListRef = useRef();
+
+  const onWheel = (e) => {
+    if (e.deltaY > 0) {
+      tagListRef.current.scrollLeft += 20;
+    } else {
+      tagListRef.current.scrollLeft -= 20;
+    }
+  };
+
   return (
-    <div className='tag-list-container'>
+    <div className='tag-list-container' onWheel={onWheel} ref={tagListRef}>
       <LinearLayout direction='row'>
-        <LinearLayout direction='row'>
-          <Subtitle3>태그</Subtitle3>
-          {tags.map((tag, idx) => (
-            <Tag key={idx}>
-              <Caption1>{tag}</Caption1>
-            </Tag>
-          ))}
-        </LinearLayout>
-        <LinearLayout direction='row'>
-          <button className='room-minimize'>
-            <IoRemove size='24px' />
-          </button>
-          <button className='room-exit'>
-            <IoCloseOutline size='24px' />
-          </button>
-        </LinearLayout>
+        {tags.map((tag, index) => (
+          <Tag key={index}>
+            <Caption1>{tag}</Caption1>
+          </Tag>
+        ))}
       </LinearLayout>
     </div>
   );
