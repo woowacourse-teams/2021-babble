5e380ab4dbc66f0e9f94b9d9df8bd281796f82c1
Min Kyung Kang <mnk918p@gmail.com>
2021-07-17 22:06:38 +0900

    feat: 방 생성 및 방 참가 API 연결

--- front/src/pages/RoomList/RoomList.js
+++ front/src/pages/RoomList/RoomList.js
@@ -5,32 +5,53 @@ import Logo from '../../core/Logo/Logo';
 import ModalChattingRoom from '../../components/Modal/ModalChattingRoom';
 import React from 'react';
 import SquareButton from '../../components/Button/SquareButton';
-import data from '../../../mockData.json';
-// import axios from 'axios';
+import axios from 'axios';
 import { useModal } from '../../contexts/ModalProvider';
 
 const RoomList = () => {
   const { open } = useModal();
 
-  const openChatting = async () => {
+  const createChatting = async () => {
     try {
-      // const response = await axios.get('/mockData.json');
-      // const data = await response.json();
-      const {
-        tags,
-        host: { id, name, avatar },
-        roomId,
-        createdDate,
-      } = data.roomInfo;
+      const response = await axios.post(
+        'https://babble-test.o-r.kr/api/rooms',
+        {
+          gameId: 1,
+          tags: ['실버', '2시간', '솔로랭크'],
+        }
+      );
+      const { tags, roomId, createdDate } = response.data;
       open(
         <ModalChattingRoom
           tags={tags}
-          participants={{ host: { id, name, profileImg: avatar } }}
-          roomNo={roomId}
+          participants={{}}
+          roomId={roomId}
           createdAt={createdDate}
         />
-      ); // 데이터 집어넣기
+      );
     } catch (error) {
+      alert('방 생성에 실패했습니다!');
+    }
+  };
+
+  const joinChatting = async () => {
+    const roomIdInput = window.prompt('입장할 방 번호를 입력해주세요.');
+    try {
+      const response = await axios.get(
+        `https://babble-test.o-r.kr/api/rooms/${roomIdInput}`
+      );
+
+      const { tags, roomId, createdDate } = response.data;
+      open(
+        <ModalChattingRoom
+          tags={tags}
+          participants={{}}
+          roomId={roomId}
+          createdAt={createdDate}
+        />
+      );
+    } catch (error) {
+      alert('방이 존재하지 않습니다.');
       console.log(error);
     }
   };
@@ -40,10 +61,10 @@ const RoomList = () => {
       <span className='babble-logo'>
         <Logo width={100} />
       </span>
-      <SquareButton colored={false} size='large' onClick={openChatting}>
+      <SquareButton colored={false} size='large' onClick={createChatting}>
         <Body1>방 생성</Body1>
       </SquareButton>
-      <SquareButton colored={false} size='large'>
+      <SquareButton colored={false} size='large' onClick={joinChatting}>
         <Body1>방 참가</Body1>
       </SquareButton>
     </div>
