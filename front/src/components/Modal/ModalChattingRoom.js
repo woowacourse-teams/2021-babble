0bc90ed46ad463ca02dae84a70490d639e71b9e4
Min Kyung Kang <mnk918p@gmail.com>
2021-07-17 22:07:32 +0900

    feat: WebSocket과 Connect, 방 입장 시 유저 리스트 가져오는 기능 구현

--- front/src/components/Modal/ModalChattingRoom.js
+++ front/src/components/Modal/ModalChattingRoom.js
@@ -1,6 +1,6 @@
 import './Modal.scss';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import Chatbox from '../../chunks/Chatbox/Chatbox';
 import { IoCloseOutline } from 'react-icons/io5';
@@ -8,12 +8,17 @@ import { IoRemove } from 'react-icons/io5';
 import LinearLayout from '../../core/Layout/LinearLayout';
 import Participants from '../../chunks/Participants/Participants';
 import PropTypes from 'prop-types';
+import SockJS from 'sockjs-client';
+import { Stomp } from '@stomp/stompjs';
 import Subtitle3 from '../../core/Typography/Subtitle3';
 import TagList from '../../chunks/TagList/TagList';
 import { useModal } from '../../contexts/ModalProvider';
 
-const ModalChattingRoom = ({ tags, participants, roomNo, createdAt }) => {
-  const { close, minimize } = useModal();
+const ModalChattingRoom = ({ tags, participants, roomId, createdAt }) => {
+  const stompClient = useRef(null);
+  const subscription = useRef(null);
+  const userId = useRef(-1);
+  const { open, close, minimize } = useModal();
 
   const onMinimize = () => minimize();
 
@@ -21,6 +26,47 @@ const ModalChattingRoom = ({ tags, participants, roomNo, createdAt }) => {
 
   useEffect(() => {
     // 웹소켓 연결, 웹소켓으로 subscribe해서 방 참가자 정보 불러오기
+    const socket = new SockJS('https://babble-test.o-r.kr/connection');
+    stompClient.current = Stomp.over(socket);
+
+    stompClient.current.connect({}, () => {
+      const socketURL = socket._transport.url;
+      const sessionId = socketURL.substring(
+        socketURL.lastIndexOf('/') - 8,
+        socketURL.lastIndexOf('/')
+      );
+
+      subscription.current = stompClient.current.subscribe(
+        `/topic/rooms/${roomId}/users`,
+        (message) => {
+          const users = JSON.parse(message.body);
+          open(
+            <ModalChattingRoom
+              tags={tags}
+              participants={users}
+              roomId={roomId}
+              createdAt={createdAt}
+            />
+          );
+        }
+      );
+
+      userId.current = Number(prompt('유저 아이디 입력하소'));
+      stompClient.current.send(
+        `/ws/rooms/${roomId}/users`,
+        {},
+        JSON.stringify({ userId, sessionId })
+      );
+    });
+
+    return () => {
+      if (subscription.current) {
+        subscription.current.unsubscribe();
+      }
+      if (stompClient.current) {
+        stompClient.current.disconnect();
+      }
+    };
   }, []);
 
   return (
@@ -46,7 +92,7 @@ const ModalChattingRoom = ({ tags, participants, roomNo, createdAt }) => {
           <Participants participants={participants} />
         </div>
         <div className='modal-chatbox-container'>
-          <Chatbox roomNo={roomNo} createdAt={createdAt}></Chatbox>
+          <Chatbox roomId={roomId} createdAt={createdAt}></Chatbox>
         </div>
       </div>
     </div>
@@ -69,7 +115,7 @@ ModalChattingRoom.propTypes = {
       })
     ),
   }),
-  roomNo: PropTypes.number,
+  roomId: PropTypes.number,
   createdAt: PropTypes.string,
 };
 
