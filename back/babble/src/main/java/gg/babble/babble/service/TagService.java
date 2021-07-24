b2b226954b99895e72439ee0cd73fcbbed81b75b
unluckyjung <ybook2012@gmail.com>
2021-07-24 18:14:37 +0900

    feat : 모든 태그를 불러오는 기능 구현

--- back/babble/src/main/java/gg/babble/babble/service/TagService.java
+++ back/babble/src/main/java/gg/babble/babble/service/TagService.java
@@ -2,6 +2,7 @@ package gg.babble.babble.service;
 
 import gg.babble.babble.domain.Tag;
 import gg.babble.babble.domain.repository.TagRepository;
+import gg.babble.babble.dto.TagResponse;
 import gg.babble.babble.exception.BabbleNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +26,13 @@ public class TagService {
     public List<Tag> findById(List<String> names) {
         return names.stream().
                 map(this::findById)
+                .collect(Collectors.toList());
+    }
+
+    public List<TagResponse> getAllTags() {
+        List<Tag> tags = tagRepository.findAll();
+        return tags.stream()
+                .map(TagResponse::from)
                 .collect(Collectors.toList());
     }
 }
--- back/babble/src/test/java/gg/babble/babble/service/TagServiceTest.java
+++ back/babble/src/test/java/gg/babble/babble/service/TagServiceTest.java
@@ -1,22 +1,70 @@
 package gg.babble.babble.service;
 
 import gg.babble.babble.ApplicationTest;
-import gg.babble.babble.exception.BabbleNotFoundException;
+import gg.babble.babble.dto.TagResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TagServiceTest extends ApplicationTest {
 
     @Autowired
     private TagService tagService;
 
-    @DisplayName("존재하지 않는 태그면 예외 처리한다.")
+//    // data Loader class 호출을 없애길 희망.
+//    // TODO : 프로필에 테스트 data Loader에서만 처리하게 하거나, 모여서 할 이야기라서 일단 keep
+//    @Autowired
+//    private TagRepository tagRepository;
+//
+//    @BeforeEach
+//    private void prepareDummyTags() {
+//        tagRepository.save(Tag.builder()
+//                .name("2시간")
+//                .build()
+//        );
+//        tagRepository.save(Tag.builder()
+//                .name("솔로랭크")
+//                .build()
+//        );
+//        tagRepository.save(Tag.builder()
+//                .name("실버")
+//                .build()
+//        );
+//    }
+//
+//    @DisplayName("존재하지 않는 태그면 예외 처리한다.")
+//    @Test
+//    void tagNotFoundTest() {
+//        assertThatThrownBy(() -> tagService.findById("쏙옙뷁훑"))
+//                .isInstanceOf(BabbleNotFoundException.class);
+//    }
+
+    @DisplayName("태그 전체를 불러오면, DB에 존재하는 모든 태그를 불러온다.")
     @Test
-    void tagNotFoundTest() {
-        assertThatThrownBy(() -> tagService.findById("쏙옙뷁훑"))
-                .isInstanceOf(BabbleNotFoundException.class);
+    void getAllTags() {
+
+        // given
+        List<TagResponse> expectedTags = Arrays.asList(
+                TagResponse.builder()
+                        .name("2시간")
+                        .build(),
+                TagResponse.builder()
+                        .name("솔로랭크")
+                        .build(),
+                TagResponse.builder()
+                        .name("실버")
+                        .build()
+        );
+
+        // when
+        List<TagResponse> allTags = tagService.getAllTags();
+
+        // then
+        assertThat(expectedTags).usingRecursiveComparison().isEqualTo(allTags);
     }
 }
