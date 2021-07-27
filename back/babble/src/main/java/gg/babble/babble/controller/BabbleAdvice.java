4c6ccf9cdf5c76096268cfea2a8776a44675fdbb
lns13301 <rkdrbgks22@ks.ac.kr>
2021-07-27 21:46:30 +0900

    feat: logback 으로 logging 구현

--- back/babble/src/main/java/gg/babble/babble/controller/BabbleAdvice.java
+++ back/babble/src/main/java/gg/babble/babble/controller/BabbleAdvice.java
@@ -4,6 +4,8 @@ import gg.babble.babble.exception.BabbleException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,23 +14,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class BabbleAdvice {
 
+    private final String ERROR_LOG = "[ERROR] %s : %s";
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionDto> unexpectedException() {
+    public ResponseEntity<ExceptionDto> unexpectedException(Exception e) {
+        logger.warn(String.format(ERROR_LOG, e.getClass().getSimpleName(), e.getMessage()));
         return ResponseEntity.badRequest().body(new ExceptionDto("unexpected exception"));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ExceptionDto> unexpectedRuntimeException() {
+    public ResponseEntity<ExceptionDto> unexpectedRuntimeException(RuntimeException e) {
+        logger.warn(String.format(ERROR_LOG, e.getClass().getSimpleName(), e.getMessage()));
         return ResponseEntity.badRequest().body(new ExceptionDto("unexpected runtime exception"));
     }
 
     @ExceptionHandler(BabbleException.class)
     public ResponseEntity<ExceptionDto> babbleException(final BabbleException babbleException) {
+        logger.info(String.format(ERROR_LOG, babbleException.getClass().getSimpleName(), babbleException.getMessage()));
         return ResponseEntity.status(babbleException.status()).body(new ExceptionDto(babbleException.getMessage()));
     }
 
     @MessageExceptionHandler
     public ResponseEntity<ExceptionDto>  handleException(final BabbleException babbleException) {
+        logger.info(String.format(ERROR_LOG, babbleException.getClass().getSimpleName(), babbleException.getMessage()));
         return ResponseEntity.status(babbleException.status()).body(new ExceptionDto(babbleException.getMessage()));
     }
 
