e575297642f667ee4fb894c467b006771737d5d3
iborymagic <mlch8023@gmail.com>
2021-07-27 18:16:34 +0900

    fix: 닉네임 관련 로직을 컴포넌트에서 삭제

--- front/src/components/NicknameSection/NicknameSection.js
+++ front/src/components/NicknameSection/NicknameSection.js
@@ -1,27 +1,19 @@
 import './NicknameSection.scss';
 
-import React, { useEffect, useState } from 'react';
-
 import Body2 from '../../core/Typography/Body2';
 import Caption1 from '../../core/Typography/Caption1';
 import { FiEdit } from 'react-icons/fi';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 const NicknameSection = ({ nickname }) => {
-  const [customNickname, setCustomNickname] = useState('');
-
-  useEffect(() => {
-    if (!customNickname) return;
-    setCustomNickname(nickname);
-  }, []);
-
-  // customNickname이 있으면(유저가 직접 닉네임을 정하면) 말풍선 없애기. 아니면 띄우기
-
   return (
     <section className='nickname-section-container'>
-      <div className='notice-bubble'>
-        <Caption1>닉네임을 변경해주세요!</Caption1>
-      </div>
+      {!nickname && (
+        <div className='notice-bubble'>
+          <Caption1>닉네임을 변경해주세요!</Caption1>
+        </div>
+      )}
       <Body2>{nickname}</Body2>
       <button className='edit'>
         <FiEdit size='18px' />
