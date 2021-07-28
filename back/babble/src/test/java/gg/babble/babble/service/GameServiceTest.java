9bcc4b1ef7662be384e56471f86c000d8d99eb21
ROOT <junroot0909@gmail.com>
2021-07-28 15:06:37 +0900

    test: 게임 전체 조회 서비스 테스트 추가

--- back/babble/src/test/java/gg/babble/babble/service/GameServiceTest.java
+++ back/babble/src/test/java/gg/babble/babble/service/GameServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import gg.babble.babble.ApplicationTest;
 import gg.babble.babble.dto.GameImageResponse;
+import gg.babble.babble.dto.GameResponse;
 import gg.babble.babble.exception.BabbleNotFoundException;
 import java.util.Arrays;
 import java.util.List;
@@ -14,7 +15,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 class GameServiceTest extends ApplicationTest {
 
+    private static final String LEAGUE_OF_LEGENDS = "League Of Legends";
+    private static final String OVERWATCH = "Overwatch";
+    private static final String APEX_LEGEND = "Apex Legend";
     private static final String LEAGUE_OF_LEGENDS_URL = "https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends-1080x1436.jpg";
+    private static final String DEFAULT_URL = "https://static-cdn.jtvnw.net/ttv-static/404_boxart-1080x1436.jpg";
+
     @Autowired
     private GameService gameService;
 
@@ -48,6 +54,20 @@ class GameServiceTest extends ApplicationTest {
 
         // then
         assertThat(gameService.findAllGameImages()).usingRecursiveComparison()
+            .isEqualTo(expectedResponses);
+    }
+
+    @DisplayName("전체 게임 리스트를 반환한다.")
+    @Test
+    void findAllGames() {
+        // when
+        List<GameForInitResponse> expectedResponses = Arrays.asList(
+          new GameForInitResponse(1L, LEAGUE_OF_LEGENDS, 1, LEAGUE_OF_LEGENDS_URL),
+          new GameForInitResponse(2L, OVERWATCH, 0, DEFAULT_URL),
+          new GameForInitResponse(3L, APEX_LEGEND, 0, DEFAULT_URL)
+        );
+        // then
+        assertThat(gameService.findAll()).usingRecursiveComparison()
             .isEqualTo(expectedResponses);
     }
 }
