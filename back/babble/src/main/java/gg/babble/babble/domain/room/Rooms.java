151f439b334a0acfb98cfc4530b5e7ee5123b5b5
hyeon9mak <jinha3507@gmail.com>
2021-07-28 18:31:19 +0900

    feat: Rooms 일급 컬렉션 추가
    Game 엔티티와 양방향 매핑을 위해 생성
    totalHeadCount를 통해 특정게임 방들의 모든 유저 수 반환

---
+++ back/babble/src/main/java/gg/babble/babble/domain/room/Rooms.java
@@ -0,0 +1,27 @@
+package gg.babble.babble.domain.room;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+
+@Embeddable
+public class Rooms {
+
+    @OneToMany(mappedBy = "game")
+    private final List<Room> rooms;
+
+    public Rooms() {
+        this(new ArrayList<>());
+    }
+
+    public Rooms(final List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    public int totalHeadCount() {
+        return rooms.stream()
+            .mapToInt(Room::currentHeadCount)
+            .sum();
+    }
+}
