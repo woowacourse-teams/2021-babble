18a1fc3fb53ddc3d40ada61f35e63574bcef8126
unluckyjung <ybook2012@gmail.com>
2021-07-29 15:48:51 +0900

    feat: 인터셉터에서 subcribe 타입의 헤더를 가진 요청을 확인하는 기능 구현

---
+++ back/babble/src/main/java/gg/babble/babble/config/StompHandler.java
@@ -0,0 +1,34 @@
+package gg.babble.babble.config;
+
+import gg.babble.babble.service.SubscribeAuthService;
+import java.util.Objects;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+
+@Configuration
+public class StompHandler implements ChannelInterceptor {
+
+    private final SubscribeAuthService subscribeAuthService;
+
+    public StompHandler(final SubscribeAuthService subscribeAuthService) {
+        this.subscribeAuthService = subscribeAuthService;
+    }
+
+    @Override
+    public Message<?> preSend(final Message<?> message, final MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
+            subscribeValidate(message);
+        }
+        return message;
+    }
+
+    private void subscribeValidate(final Message<?> message) {
+        String destinationUrl = (String) message.getHeaders().get("simpDestination");
+        subscribeAuthService.validate(Objects.requireNonNull(destinationUrl));
+    }
+}
--- back/babble/src/main/java/gg/babble/babble/config/WebSocketConfig.java
+++ back/babble/src/main/java/gg/babble/babble/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package gg.babble.babble.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +10,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
+
+    public WebSocketConfig(StompHandler stompHandler) {
+        this.stompHandler = stompHandler;
+    }
 
     @Override
     public void configureMessageBroker(final MessageBrokerRegistry registry) {
@@ -19,5 +26,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
         registry.addEndpoint("/connection").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(final ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
