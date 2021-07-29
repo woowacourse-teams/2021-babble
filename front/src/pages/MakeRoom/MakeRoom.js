45c856ee242a1ff8264f4b465b0a232498e01651
iborymagic <mlch8023@gmail.com>
2021-07-29 17:32:23 +0900

    feat: MakeRoom 페이지 로직 구현 - 이미지 불러오기 - 태그 목록 불러오기 - 태그 선택하기 - 태그 삭제하기 - 방 생성하기 - 인원 선택 드롭다운 메뉴

--- front/src/pages/MakeRoom/MakeRoom.js
+++ front/src/pages/MakeRoom/MakeRoom.js
@@ -1,11 +1,13 @@
 import './MakeRoom.scss';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Body2 from '../../core/Typography/Body2';
+import ChattingRoom from '../ChattingRoom/ChattingRoom';
 import DropdownInput from '../../components/SearchInput/DropdownInput';
 import Headline2 from '../../core/Typography/Headline2';
 import { Link } from 'react-router-dom';
+import { MODAL_TYPE_CHATTING } from '../../constants/chat';
 import MainImage from '../../components/MainImage/MainImage';
 import PATH from '../../constants/path';
 import PageLayout from '../../core/Layout/PageLayout';
@@ -17,17 +19,69 @@ import axios from 'axios';
 
 const MakeRoom = ({ gameId }) => {
   const [imageUrl, setImageUrl] = useState('');
+  const [tagList, setTagList] = useState([]);
+  const [selectedTagList, setSelectedTagList] = useState([]);
+  const [headCount, setHeadCount] = useState(0);
 
   const getImage = async () => {
-    const mainImage = await axios.get(
+    const { image } = await axios.get(
       `https://babble.o-r.kr/api/games/${gameId}/images`
     );
 
-    setImageUrl(mainImage);
+    setImageUrl(image);
+  };
+
+  const getTags = async () => {
+    const tags = await axios.get('https://babble.o-r.kr/api/tags');
+
+    setTagList(tags);
+  };
+
+  const selectTag = (tagName) => {
+    const tagId = tagList.find((tag) => tag.name === tagName).id;
+    const tag = { id: tagId, name: tagName };
+
+    setSelectedTagList((prevTagList) => [...prevTagList, tag]);
+  };
+
+  const eraseTag = (e) => {
+    // TODO: selectedTag 찾는 로직 고민해보기(element 구조에 종속적임)
+    const selectedTag = e.target
+      .closest('.tag-container')
+      .querySelector('.caption1').textContent;
+
+    setSelectedTagList((prevTagList) => [
+      ...prevTagList.filter((tag) => tag !== selectedTag),
+    ]);
+  };
+
+  const createRoom = async () => {
+    try {
+      // TODO: 테스트 서버에서 실제 배포 서버로 변경하기
+      const response = await axios.post('https://babble.o-r.kr/api/rooms', {
+        gameId,
+        maxHeadCount: headCount,
+        tags: selectedTagList,
+      });
+      const { tags, roomId, createdDate } = response.data;
+
+      open(
+        <ChattingRoom
+          tags={tags}
+          participants={{}}
+          roomId={roomId}
+          createdAt={createdDate}
+        />,
+        MODAL_TYPE_CHATTING
+      );
+    } catch (error) {
+      alert('방 생성을 하는 데 오류가 있습니다.');
+    }
   };
 
   useEffect(() => {
     getImage();
+    getTags();
   }, []);
 
   return (
@@ -36,20 +90,18 @@ const MakeRoom = ({ gameId }) => {
       <PageLayout type='narrow'>
         <Headline2>방 생성하기</Headline2>
         <section className='inputs'>
-          <DropdownInput dropdownKeywords={[]} />
-          <SearchInput autoCompleteKeywords={[]} />
+          <DropdownInput
+            dropdownKeywords={[...Array(21).keys()].slice(2)}
+            headCount={headCount}
+            setHeadCount={setHeadCount}
+          />
+          <SearchInput
+            autoCompleteKeywords={tagList}
+            onClickKeyword={selectTag}
+          />
         </section>
         <section className='tag-list-section'>
-          <TagList
-            tags={[
-              { name: '실버' },
-              { name: '골드 승급전' },
-              { name: '원딜' },
-              { name: '솔로랭크' },
-              { name: '음성채팅 가능' },
-            ]}
-            erasable
-          />
+          <TagList tags={selectedTagList} onDeleteTag={eraseTag} erasable />
         </section>
         <section className='buttons'>
           <Link to={PATH.HOME}>
@@ -58,7 +110,7 @@ const MakeRoom = ({ gameId }) => {
             </RoundButton>
           </Link>
           <Link to={PATH.HOME}>
-            <RoundButton colored={true} size='small' onClick={() => {}}>
+            <RoundButton colored={true} size='small' onClick={createRoom}>
               <Body2>생성하기</Body2>
             </RoundButton>
           </Link>
