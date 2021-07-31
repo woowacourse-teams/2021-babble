46fd87849e6a9e0042bd72b27aa451dc05ec5f2c
iborymagic <mlch8023@gmail.com>
2021-07-31 17:16:08 +0900

    feat: HOST와 본인을 인식시켜주는 뱃지 기능 추가

--- front/src/chunks/Participants/Participants.js
+++ front/src/chunks/Participants/Participants.js
@@ -1,13 +1,19 @@
 import './Participants.scss';
 
 import Avatar from '../../components/Avatar/Avatar';
+import Badge from '../../components/Badge/Badge';
 import Caption2 from '../../core/Typography/Caption2';
 import LinearLayout from '../../core/Layout/LinearLayout';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { RiVipCrown2Fill } from 'react-icons/ri';
 import Subtitle3 from '../../core/Typography/Subtitle3';
+import { VscCircleFilled } from 'react-icons/vsc';
+import { useUser } from '../../contexts/UserProvider';
 
 const Participants = ({ participants }) => {
+  const { user } = useUser();
+
   return (
     <aside className='participants-container'>
       <Subtitle3>참가자</Subtitle3>
@@ -15,12 +21,19 @@ const Participants = ({ participants }) => {
         {participants?.host && (
           <Avatar direction='row' imageSrc={participants?.host?.profileImg}>
             <Caption2>{participants?.host?.nickname}</Caption2>
+            <Badge colored={user.id === participants?.host?.id}>
+              <RiVipCrown2Fill size='12px' />
+            </Badge>
           </Avatar>
         )}
-
-        {participants?.guests?.map(({ profileImg, nickname }, index) => (
+        {participants?.guests?.map(({ id, profileImg, nickname }, index) => (
           <Avatar direction='row' key={index} imageSrc={profileImg}>
             <Caption2>{nickname}</Caption2>
+            {user.id === id && (
+              <Badge colored>
+                <VscCircleFilled size='12px' />
+              </Badge>
+            )}
           </Avatar>
         ))}
       </LinearLayout>
