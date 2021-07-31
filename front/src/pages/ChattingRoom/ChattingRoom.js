1b882130b482aa78b9d388b71151402f40e25358
iborymagic <mlch8023@gmail.com>
2021-07-31 18:11:08 +0900

    refactor: connection guard를 stompClient.current가 아닌 별도의 플래그로 관리

--- front/src/pages/ChattingRoom/ChattingRoom.js
+++ front/src/pages/ChattingRoom/ChattingRoom.js
@@ -27,6 +27,8 @@ const ChattingRoom = ({ tags, roomId, createdAt }) => {
   const stompClient = useRef(null);
   const user_subscription = useRef(null);
   const chat_subscription = useRef(null);
+  const isConnected = useRef(false);
+
   const { close, minimize } = useChattingModal();
   const {
     user: { nickname, id: userId },
@@ -37,18 +39,21 @@ const ChattingRoom = ({ tags, roomId, createdAt }) => {
 
   const onClose = () => close();
 
+  const sendMessage = (content) => {
+    stompClient.current.send(
+      `/ws/rooms/${roomId}/chat`,
+      {},
+      JSON.stringify({ userId, content })
+    );
+  };
+
   const onSubmit = (e) => {
     e.preventDefault();
 
     const content = e.target.chat.value;
     if (!content) return;
 
-    stompClient.current.send(
-      `/ws/rooms/${roomId}/chat`,
-      {},
-      JSON.stringify({ userId, content })
-    );
-
+    sendMessage(content);
     e.target.reset();
   };
 
@@ -71,8 +76,10 @@ const ChattingRoom = ({ tags, roomId, createdAt }) => {
       if (chat_subscription.current) {
         chat_subscription.current.unsubscribe();
       }
-      if (stompClient.current) {
+      if (isConnected.current) {
+        sendMessage(`${nickname} 님이 퇴장했습니다!`);
         stompClient.current.disconnect();
+        isConnected.current = false;
       }
     };
   }, []);
@@ -81,9 +88,13 @@ const ChattingRoom = ({ tags, roomId, createdAt }) => {
     const socket = new SockJS('https://babble-test.o-r.kr/connection');
     stompClient.current = Stomp.over(socket);
 
-    if (stompClient.current) stompClient.current.disconnect();
+    if (isConnected.current) {
+      stompClient.current.disconnect();
+      isConnected.current = false;
+    }
 
     stompClient.current.connect({}, () => {
+      isConnected.current = true;
       const socketURL = socket._transport.url;
       const sessionId = socketURL.substring(
         socketURL.lastIndexOf(SOCKET_URL_DIVIDER) - SESSION_ID_LENGTH,
@@ -106,6 +117,13 @@ const ChattingRoom = ({ tags, roomId, createdAt }) => {
         `/topic/rooms/${roomId}/chat`,
         (message) => {
           setChattings((prevChattings) => [
+            // setChatting를 해줄 때 어떤 조건을 기준으로 type을 나눠줄 것이냐.
+            // (나중에 chattings에서 메시지를 꺼내서 사용할 때 type에 따라 렌더링을 해줘야 하니깐)
+
+            // 1. 입장 시 가장 먼저 배열에 들어온 chatting이 입장 메시지.
+            // 2. 서버에서 Chatting Type을 지정해준다. (announce, defaultChat)
+            // 3. 서버에서 Announce API를 따로 만든다.
+
             ...prevChattings,
             {
               user: JSON.parse(message.body).user,
@@ -120,6 +138,8 @@ const ChattingRoom = ({ tags, roomId, createdAt }) => {
         {},
         JSON.stringify({ userId, sessionId })
       );
+
+      sendMessage(`${nickname} 님이 입장했습니다!`);
     });
   }, [userId]);
 
