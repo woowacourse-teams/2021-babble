7418463273f9c8cd367b58d93918a67dd6170d4d
iborymagic <mlch8023@gmail.com>
2021-08-05 02:36:31 +0900

    feat: GameList 페이지 구현
    Co-authored-by: Min Kyung Kang <mnk918p@gmail.com>

--- front/src/App.js
+++ front/src/App.js
@@ -2,22 +2,26 @@ import '../global.scss';
 
 import { Route, Switch } from 'react-router-dom';
 
+import GameList from './pages/GameList/GameList';
 import MakeRoom from './pages/MakeRoom/MakeRoom';
+import { NavBar } from './components';
 import PATH from './constants/path';
 import React from 'react';
 import RoomList from './pages/RoomList/RoomList';
 
 const App = () => {
   return (
-    <Switch>
-      <Route path={PATH.HOME} exact>
-        <RoomList gameId={1} />
-      </Route>
-      <Route path={PATH.MAKE_ROOM}>
-        {/* TODO: 나중에 gameId를 선택하는 게임에 따라 바뀌도록 로직 추가하기 */}
-        <MakeRoom gameId={1} />
-      </Route>
-    </Switch>
+    <>
+      <NavBar />
+      <Switch>
+        <Route path={PATH.HOME} component={GameList} exact />
+        <Route
+          path={`${PATH.ROOM_LIST}/:gameId${PATH.MAKE_ROOM}`}
+          component={MakeRoom}
+        />
+        <Route path={`${PATH.ROOM_LIST}/:gameId`} component={RoomList} />
+      </Switch>
+    </>
   );
 };
 
