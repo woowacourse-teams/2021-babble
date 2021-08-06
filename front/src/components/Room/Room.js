dde041de74160b262b541a3b9cb2e9e3a47b62a2
iborymagic <mlch8023@gmail.com>
2021-08-07 00:29:01 +0900

    css: 현재 접속중인 방과 인원이 가득찬 방에 재접속 불가능 여부를 표시해주는 스타일 추가
    Co-authored-by: Min Kyung Kang <mnk918p@gmail.com>

--- front/src/components/Room/Room.js
+++ front/src/components/Room/Room.js
@@ -5,36 +5,45 @@ import { Body2 } from '../../core/Typography';
 import PropTypes from 'prop-types';
 import React from 'react';
 import TagList from '../../chunks/TagList/TagList';
+import { useUser } from '../../contexts/UserProvider';
 
 const Room = ({ imageSrc, room, onClickRoom }) => {
   const { roomId, host, headCount, tags } = room;
   const { current, max } = headCount;
+  const {
+    user: { currentRoomNumber },
+  } = useUser();
+
+  const isDisabled = currentRoomNumber === roomId || max === current;
 
   return (
-    <section
-      className='room-container'
-      data-room-id={roomId}
-      onClick={onClickRoom}
-    >
-      <section className='information'>
-        <div className='about'>
-          <div className='room-number'>
-            <Body2>{roomId}</Body2>
+    <div className='room-wrapper'>
+      <div className={`room-shade ${isDisabled ? 'disabled' : ''}`}></div>
+      <section
+        className={`room-container ${isDisabled ? 'disabled' : ''}`}
+        data-room-id={roomId}
+        onClick={isDisabled ? null : onClickRoom}
+      >
+        <section className='information'>
+          <div className='about'>
+            <div className='room-number'>
+              <Body2>{roomId}</Body2>
+            </div>
+            <div className='avatar'>
+              <Avatar size='small' imageSrc={imageSrc} direction='row'>
+                <Body2>{host.nickname}</Body2>
+              </Avatar>
+            </div>
+            <TagList tags={tags} />
           </div>
-          <div className='avatar'>
-            <Avatar size='small' imageSrc={imageSrc} direction='row'>
-              <Body2>{host.nickname}</Body2>
-            </Avatar>
+          <div className='headcount-container'>
+            <Body2>
+              <strong>{current}</strong> / {max}
+            </Body2>
           </div>
-          <TagList tags={tags} />
-        </div>
-        <div className='headcount-container'>
-          <Body2>
-            <strong>{current}</strong> / {max}
-          </Body2>
-        </div>
+        </section>
       </section>
-    </section>
+    </div>
   );
 };
 
