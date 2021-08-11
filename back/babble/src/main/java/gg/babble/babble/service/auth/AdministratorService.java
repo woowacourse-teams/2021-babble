347dcf6380e23654999fb6fc6f2742ee3c49e598
ROOT <junroot0909@gmail.com>
2021-08-11 15:41:43 +0900

    관리자 필터 기능 구현 (#410)
    * feat: 관리자 기능 API IP 필터링 구현
    
    * test: 인증된 IP로 게임 등록 TC 추가
    
    * feat: 관리자 ip 전체 조회 및 삭제 구현
    
    * feat: 관리자 IP 전체 조회는 허가된 IP만 가능하도록 구현
    
    * refactor: CommandLineRunner 없이 테스트하도록 수정
    
    * feat: 관리자 Ip 추가 기능 구현
    
    * docs: 추가 구현 사항 REST docs 반영
    
    * chore: main 브랜치에도 소나큐브가 적용되도록 수정
    
    * feat: 관리자 Ip Unique 속성 추가
    
    웹 소켓 테스트의 경우 IP 중복 등록하는 이슈가 발견되어 상위 클래스를 분리함
    
    * docs: 변경 사항 Rest docs 반영

---
+++ back/babble/src/main/java/gg/babble/babble/service/auth/AdministratorService.java
@@ -0,0 +1,52 @@
+package gg.babble.babble.service.auth;
+
+import gg.babble.babble.domain.admin.Administrator;
+import gg.babble.babble.domain.admin.Ip;
+import gg.babble.babble.domain.repository.AdministratorRepository;
+import gg.babble.babble.dto.request.AdministratorRequest;
+import gg.babble.babble.dto.response.AdministratorResponse;
+import gg.babble.babble.exception.BabbleAuthenticationException;
+import gg.babble.babble.exception.BabbleNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@Service
+public class AdministratorService {
+
+    private final AdministratorRepository administratorRepository;
+
+    public AdministratorService(final AdministratorRepository administratorRepository) {
+        this.administratorRepository = administratorRepository;
+    }
+
+    public void validateIp(final String ip) {
+        if (!administratorRepository.existsAdministratorByIp(new Ip(ip))) {
+            throw new BabbleAuthenticationException(String.format("허용되지 않는 Ip입니다. (현재 Ip: %s)", ip));
+        }
+    }
+
+    @Transactional
+    public void deleteAdministrator(final Long id) {
+        Administrator administrator = findById(id);
+        administratorRepository.delete(administrator);
+    }
+
+    private Administrator findById(final Long id) {
+        return administratorRepository.findById(id)
+            .orElseThrow(() -> new BabbleNotFoundException(String.format("존재하지 않는 관리자 Id(%d) 입니다.", id)));
+    }
+
+    public List<AdministratorResponse> findAll() {
+        return administratorRepository.findAll()
+            .stream()
+            .map(AdministratorResponse::from)
+            .collect(Collectors.toList());
+    }
+
+    public AdministratorResponse insert(final AdministratorRequest request) {
+        return AdministratorResponse.from(administratorRepository.save(new Administrator(request.getIp(), request.getName())));
+    }
+}
