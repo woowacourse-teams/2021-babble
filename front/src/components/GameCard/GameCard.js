1e7df826867a7d8f44e910f9180fc3a2fb5cf16b
Min Kyung Kang <mnk918p@gmail.com>
2021-09-02 18:42:22 +0900

    fix: GameCard 애니메이션 단순화
    Co-authored-by: An Hyun Cheol <mlch8023@gmail.com>

--- front/src/components/GameCard/GameCard.js
+++ front/src/components/GameCard/GameCard.js
@@ -1,56 +1,14 @@
 import './GameCard.scss';
 
 import { Body2, Caption1 } from '../../core/Typography';
-import React, { useRef } from 'react';
 
 import PropTypes from 'prop-types';
+import React from 'react';
 
 const GameCard = ({ gameName, imageSrc, participants, ...rest }) => {
-  const thumbnailRef = useRef(null);
-
-  const onImageMouseDown = () => {
-    thumbnailRef.current.classList.add('active');
-  };
-
-  const onImageMouseUp = () => {
-    thumbnailRef.current.classList.remove('active');
-  };
-
-  const onImageMouseLeave = () => {
-    thumbnailRef.current.classList.remove('center', 'right', 'left', 'active');
-  };
-
-  const onImageMouseMove = (e) => {
-    thumbnailRef.current.classList.remove('center', 'right', 'left', 'active');
-
-    const leftOffset = thumbnailRef.current.getBoundingClientRect().left;
-    const imageWidth = thumbnailRef.current.offsetWidth;
-    const myPosX = e.pageX;
-    const clearedClassList = thumbnailRef.current.className
-      .replace(/center|right|left/gi, '')
-      .trim();
-
-    if (myPosX < leftOffset + 0.3 * imageWidth) {
-      thumbnailRef.current.className = `${clearedClassList} left`;
-    } else {
-      if (myPosX > leftOffset + 0.65 * imageWidth) {
-        thumbnailRef.current.className = `${clearedClassList} right`;
-      } else {
-        thumbnailRef.current.className = `${clearedClassList} center`;
-      }
-    }
-  };
-
   return (
     <figure className='game-card-container' {...rest}>
-      <div
-        className='thumbnail'
-        ref={thumbnailRef}
-        onMouseDown={onImageMouseDown}
-        onMouseUp={onImageMouseUp}
-        onMouseMove={onImageMouseMove}
-        onMouseOut={onImageMouseLeave}
-      >
+      <div className='thumbnail'>
         <img src={imageSrc} alt={`${gameName} image`} />
       </div>
       <figcaption>
