fcb013dff4899bce11b3733a2df79a6ae84e3635
ROOT <junroot0909@gmail.com>
2021-09-03 20:20:58 +0900

    feat: 태그 대체 이름 flyway 적용

--- back/babble/src/main/resources/db/migration/V5__add_table_alternativeGameName.sql
+++ back/babble/src/main/resources/db/migration/V5__add_table_alternativeGameName.sql
@@ -5,3 +5,6 @@ create table alternative_game_name
     game_id bigint       not null,
     primary key (id)
 );
+
+alter table alternative_game_name
+    add constraint fk_alternative_game_name_game foreign key (game_id) references game(id);
