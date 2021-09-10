069f19488e8341a046db1c5c8688f3ba8d3bbe9c
Min Kyung Kang <mnk918p@gmail.com>
2021-09-11 02:38:40 +0900

    chore: 코드 최적화 (#516)
    * feat: 줄임말에 대한 자동완성 기능 구현
    
    Co-authored-by: An Hyun Cheol <iborymagic@users.noreply.github.com>
    
    * fix: 프로필 이미지 버그 수정
    
    Co-authored-by: An Hyun Cheol <iborymagic@users.noreply.github.com>
    
    * fix: useThrottle에 clearTimeout 추가
    
    Co-authored-by: An Hyun Cheol <iborymagic@users.noreply.github.com>
    
    * refactor: Code splitting 및 chunking
    
    Co-authored-by: An Hyun Cheol <iborymagic@users.noreply.github.com>
    
    * chore: react-icons tree shaking, gzip 압축 적용
    
    Co-authored-by: An Hyun Cheol <iborymagic@users.noreply.github.com>
    
    Co-authored-by: An Hyun Cheol <iborymagic@users.noreply.github.com>

--- front/src/components/Avatar/AvatarImage.js
+++ front/src/components/Avatar/AvatarImage.js
@@ -5,7 +5,9 @@ import React from 'react';
 
 const AvatarImage = ({ imageSrc, size }) => {
   return (
-    <img src={imageSrc} alt='Avatar Image' className={`avatar-image ${size}`} />
+    <div className={`avatar-image-container ${size}`}>
+      <img src={imageSrc} alt='Avatar Image' className='avatar-image' />
+    </div>
   );
 };
 
