3b21df8e8a3d0939ffe10290b6b0388239f68e16
ROOT <junroot0909@gmail.com>
2021-09-29 15:05:37 +0900

    feat: x-forwarded-for 헤더를 사용해서 IP를 확인하도록 수정

--- back/babble/src/main/java/gg/babble/babble/config/AdminAccessInterceptor.java
+++ back/babble/src/main/java/gg/babble/babble/config/AdminAccessInterceptor.java
@@ -2,12 +2,14 @@ package gg.babble.babble.config;
 
 import gg.babble.babble.service.auth.AdministratorService;
 import gg.babble.babble.util.UrlParser;
+import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class AdminAccessInterceptor implements HandlerInterceptor {
 
+    private static final String X_FORWARDED_FOR_HEADER = "x-forwarded-for";
     private final AdministratorService administratorService;
 
     public AdminAccessInterceptor(final AdministratorService administratorService) {
@@ -17,10 +19,21 @@ public class AdminAccessInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) {
         if (needsAuthentication(request)) {
-            String clientIp = request.getRemoteAddr();
+            String clientIp = getClientIpFrom(request);
+
             administratorService.validateIp(clientIp);
         }
         return true;
+    }
+
+    private String getClientIpFrom(final HttpServletRequest request) {
+        String clientIp = request.getHeader(X_FORWARDED_FOR_HEADER);
+
+        if (Objects.isNull(clientIp)) {
+            clientIp = request.getRemoteAddr();
+        }
+
+        return clientIp;
     }
 
     private boolean needsAuthentication(final HttpServletRequest request) {
