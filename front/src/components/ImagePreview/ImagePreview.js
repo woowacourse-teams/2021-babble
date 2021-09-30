79e782e3a9ca321a310a7d78115bb3c673499995
Min Kyung Kang <mnk918p@gmail.com>
2021-10-01 01:57:32 +0900

    feat: 관리자페이지 이미지 프리뷰 컴포넌트 구현

---
+++ front/src/components/ImagePreview/ImagePreview.js
@@ -0,0 +1,32 @@
+import './ImagePreview.scss';
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Subtitle3 } from '../../core/Typography';
+
+const ImagePreview = ({ imageList }) => {
+  const { bigImage, middleImage, smallImage } = imageList;
+
+  return (
+    <section className='image-preview-section'>
+      <Subtitle3>이미지 Preview</Subtitle3>
+      <picture>
+        <div className='preview'>
+          <img src={bigImage} alt='' />
+        </div>
+        <div className='preview'>
+          <img src={middleImage} alt='' />
+        </div>
+        <div className='preview'>
+          <img src={smallImage} alt='' />
+        </div>
+      </picture>
+    </section>
+  );
+};
+
+export default ImagePreview;
+
+ImagePreview.propTypes = {
+  imageList: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
