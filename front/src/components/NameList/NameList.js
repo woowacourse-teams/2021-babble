2f41f524503637d5f523f640aaaf2c97636902a0
Min Kyung Kang <mnk918p@gmail.com>
2021-10-02 02:02:00 +0900

    feat: 조회된 데이터가 없을 경우에 대한 alert 추가

--- front/src/components/NameList/NameList.js
+++ front/src/components/NameList/NameList.js
@@ -5,18 +5,22 @@ import { Tag, TagErasable } from '..';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const NameList = ({ list, erasable, onClickNames }) => {
+const NameList = ({ list, erasable, onClickNames = null }) => {
   return (
     <div className='name-list-container'>
-      {list.map((itemName, index) =>
-        erasable ? (
-          <TagErasable onClickTagName={onClickNames} key={index}>
-            {itemName}
-          </TagErasable>
-        ) : (
-          <Tag onClickTagName={onClickNames} key={index}>
-            {itemName}
-          </Tag>
+      {list.length === 0 ? (
+        <div className='no-data'>조회된 데이터가 없습니다.</div>
+      ) : (
+        list.map((item) =>
+          erasable ? (
+            <TagErasable onClickTagName={onClickNames} key={item.id}>
+              {item.name}
+            </TagErasable>
+          ) : (
+            <Tag onClickTagName={onClickNames} key={item.id}>
+              {item.name}
+            </Tag>
+          )
         )
       )}
     </div>
@@ -24,7 +28,12 @@ const NameList = ({ list, erasable, onClickNames }) => {
 };
 
 NameList.propTypes = {
-  list: PropTypes.arrayOf(PropTypes.string),
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
   erasable: PropTypes.bool,
   onClickNames: PropTypes.func,
 };
