c24a214e3a774062f4b69632514d833c8812e628
iborymagic <mlch8023@gmail.com>
2021-10-01 18:06:44 +0900

    feat: 운영자 관리 컴포넌트 마크업 구현

--- front/src/components/NameList/NameList.js
+++ front/src/components/NameList/NameList.js
@@ -5,17 +5,17 @@ import { Tag, TagErasable } from '..';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const NameList = ({ list, erasable, onClickNames }) => {
+const NameList = ({ list, erasable, onClickNames = null }) => {
   return (
     <div className='name-list-container'>
-      {list.map((itemName, index) =>
+      {list.map((item) =>
         erasable ? (
-          <TagErasable onClickTagName={onClickNames} key={index}>
-            {itemName}
+          <TagErasable onClickTagName={onClickNames} key={item.id}>
+            {item.name}
           </TagErasable>
         ) : (
-          <Tag onClickTagName={onClickNames} key={index}>
-            {itemName}
+          <Tag onClickTagName={onClickNames} key={item.id}>
+            {item.name}
           </Tag>
         )
       )}
@@ -24,7 +24,12 @@ const NameList = ({ list, erasable, onClickNames }) => {
 };
 
 NameList.propTypes = {
-  list: PropTypes.arrayOf(PropTypes.string),
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
   erasable: PropTypes.bool,
   onClickNames: PropTypes.func,
 };
