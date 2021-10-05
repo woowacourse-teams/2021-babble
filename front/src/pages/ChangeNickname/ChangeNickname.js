f4ec6a8fae9680b8d18c45d4fcb3f9f1c915b7c9
Min Kyung Kang <mnk918p@gmail.com>
2021-10-05 14:51:05 +0900

    fix: error message 보여주기

--- front/src/pages/ChangeNickname/ChangeNickname.js
+++ front/src/pages/ChangeNickname/ChangeNickname.js
@@ -1,19 +1,21 @@
 import './ChangeNickname.scss';
 
 import { Caption1, Subtitle3 } from '../../core/Typography';
-import { ModalError, RoundButton, TextInput } from '../../components';
 import { NICKNAME_MAX_LENGTH, NICKNAME_MIN_LENGTH } from '../../constants/chat';
+import React, { useState } from 'react';
+import { RoundButton, TextInput } from '../../components';
 
 import { BASE_URL } from '../../constants/api';
 import { IoCloseOutline } from '@react-icons/all-files/io5/IoCloseOutline';
-import React from 'react';
 import axios from 'axios';
 import { setSessionStorage } from '../../utils/storage';
 import { useDefaultModal } from '../../contexts/DefaultModalProvider';
 import { useUser } from '../../contexts/UserProvider';
 
 const ChangeNickname = () => {
-  const { openModal, closeModal } = useDefaultModal();
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const { closeModal } = useDefaultModal();
   const { user, changeUser, setIsNicknameChanged } = useUser();
 
   const submitNickname = async (e) => {
@@ -32,7 +34,12 @@ const ChangeNickname = () => {
       changeUser({ id: generatedUser.id, nickname: generatedUser.nickname });
       closeModal();
     } catch (error) {
-      openModal(<ModalError>{error}</ModalError>);
+      //TODO: 상수화 필요
+      setErrorMessage(
+        error.response.data[0].message
+          .split('닉네임은 ')[1]
+          .split('입력 닉네임:')[0]
+      );
     }
   };
 
@@ -52,6 +59,7 @@ const ChangeNickname = () => {
           isContentSelected
           autocomplete='off'
         />
+        <span className='form-error'>{errorMessage}</span>
       </div>
       <div className='control-buttons'>
         <RoundButton onClick={closeModal} size='small'>
