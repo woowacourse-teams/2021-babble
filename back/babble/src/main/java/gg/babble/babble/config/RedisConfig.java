dc50d97b6c65254a5a20bd6491314f07f1efe646
ROOT <junroot0909@gmail.com>
2021-10-19 17:33:12 +0900

    refactor: 사용하지 않는 import 제거

--- back/babble/src/main/java/gg/babble/babble/config/RedisConfig.java
+++ back/babble/src/main/java/gg/babble/babble/config/RedisConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
