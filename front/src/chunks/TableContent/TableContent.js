fba1e89d0ac003a2ed7a5119d91612a3e4def60c
Min Kyung Kang <mnk918p@gmail.com>
2021-10-20 13:39:17 +0900

    feat: Table Content 구현

---
+++ front/src/chunks/TableContent/TableContent.js
@@ -0,0 +1,68 @@
+import './TableContent.scss';
+
+import { Body2, Caption2 } from '../../core/Typography';
+
+import { AiTwotonePushpin } from '@react-icons/all-files/ai/AiTwotonePushpin';
+import { BiTimeFive } from '@react-icons/all-files/bi/BiTimeFive';
+import { BsPersonFill } from '@react-icons/all-files/bs/BsPersonFill';
+import { FaHotjar } from '@react-icons/all-files/fa/FaHotjar';
+import InfoWithIcon from '../../components/InfoWithIcon/InfoWithIcon';
+import LikeAndView from '../../chunks/LikeAndView/LikeAndView';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const TableContent = ({
+  boardDetails,
+  onContentClick = () => {},
+  onCategoryClick = () => {},
+}) => {
+  const { title, category, viewCount, likeCount, author, createdAt, notice } =
+    boardDetails;
+  return (
+    <main className='table-content-container' onClick={onContentClick}>
+      <div className='table-content-details'>
+        {notice && (
+          <InfoWithIcon
+            icon={<AiTwotonePushpin size='22px' />}
+            color='babble-pink'
+          />
+        )}
+        {likeCount > 10 && !notice && (
+          <InfoWithIcon icon={<FaHotjar size='18px' />} color='red' />
+        )}
+
+        <div>
+          <Body2>{title}</Body2>
+
+          <span className='table-infos'>
+            <button onClick={onCategoryClick} className='category-search'>
+              <Caption2>{category}</Caption2>
+            </button>
+            <LikeAndView view={viewCount} like={likeCount} />
+            <InfoWithIcon
+              icon={<BsPersonFill size='15px' />}
+              content={author}
+            />
+          </span>
+        </div>
+      </div>
+      <InfoWithIcon icon={<BiTimeFive size='15px' />} content={createdAt} />
+    </main>
+  );
+};
+
+TableContent.propTypes = {
+  boardDetails: PropTypes.shape({
+    title: PropTypes.string,
+    category: PropTypes.string,
+    author: PropTypes.string,
+    createdAt: PropTypes.string,
+    viewCount: PropTypes.string,
+    likeCount: PropTypes.string,
+    notice: PropTypes.bool,
+  }),
+  onContentClick: PropTypes.func,
+  onCategoryClick: PropTypes.func,
+};
+
+export default TableContent;
---
+++ front/src/chunks/TableContent/TableContent.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TableContent from './TableContent';
+
+export default {
+  title: 'chunks/TableContent',
+  component: TableContent,
+};
+
+const TableContentTemplate = (args) => (
+  <div>
+    <TableContent {...args} />
+  </div>
+);
+
+export const Default = TableContentTemplate.bind({});
+
+Default.args = {
+  boardDetails: {
+    title: '롤드컵 존잼;; ㄷㄷ',
+    category: '자유',
+    author: '그루밍',
+    createdAt: '10/18 09:23',
+    viewCount: '18234',
+    likeCount: '2918',
+  },
+};
