f5b8fc02d897de4b5fe5358994670b9006d26407
Min Kyung Kang <mnk918p@gmail.com>
2021-10-21 22:24:11 +0900

    fix: Search Button Click시 동작을 수행할 수 있도록 함수 구현

--- front/src/components/SearchInput/TextSearchInput.js
+++ front/src/components/SearchInput/TextSearchInput.js
@@ -2,7 +2,10 @@ import { FiSearch } from '@react-icons/all-files/fi/FiSearch';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const TextSearchInput = ({ placeholder = '키워드를 입력해주세요.' }) => {
+const TextSearchInput = ({
+  placeholder = '키워드를 입력해주세요.',
+  onSearchButtonClick = () => {},
+}) => {
   return (
     <div className='input-container'>
       <input
@@ -10,13 +13,16 @@ const TextSearchInput = ({ placeholder = '키워드를 입력해주세요.' }) =
         className='input-inner text-search'
         placeholder={placeholder}
       />
-      <FiSearch size='20px' />
+      <button onClick={onSearchButtonClick}>
+        <FiSearch size='20px' />
+      </button>
     </div>
   );
 };
 
 TextSearchInput.propTypes = {
   placeholder: PropTypes.string,
+  onSearchButtonClick: PropTypes.func,
 };
 
 export default TextSearchInput;
