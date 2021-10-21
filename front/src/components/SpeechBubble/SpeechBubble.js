aa14725b84cb13df6df44a737eb71bc0e752ab53
Min Kyung Kang <mnk918p@gmail.com>
2021-10-21 20:24:54 +0900

    refactor: 채팅 방에 링크 올릴 수 있도록 수정

--- front/src/components/SpeechBubble/SpeechBubble.js
+++ front/src/components/SpeechBubble/SpeechBubble.js
@@ -4,10 +4,22 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const SpeechBubble = ({ time, type = 'mine', children }) => {
+  const chat = { children: null };
+
+  if (children.includes('https://') || children.includes('http://')) {
+    chat.children = (
+      <a href={children} target='_blank' rel='noopener noreferrer'>
+        {children}
+      </a>
+    );
+    console.log(chat);
+  } else {
+    chat.children = children;
+  }
   return (
     <div className={`speech-bubble-container ${type}-container`}>
       <div className={`speech-bubble ${type}`}>
-        <pre className='text'>{children}</pre>
+        <pre className='text'>{chat.children}</pre>
         <time className='time'>{time}</time>
       </div>
     </div>
