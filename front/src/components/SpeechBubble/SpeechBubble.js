6c399ecc7cbd330bc691feac1830579b5d4c4308
Min Kyung Kang <mnk918p@gmail.com>
2021-10-22 15:05:29 +0900

    refactor: 채팅 방에서 링크 올릴 수 있도록 수정 (#630)
    * merge: merge with origin
    
    * refactor: 채팅 방에 링크 올릴 수 있도록 수정
    
    * refactor: 채팅방 카카오 버튼 위치 수정
    
    * fix: 기존 develop branch와 fetch되지 않은 내용 수정
    
    * refactor: 다른 내용과 함께 쳤을 때에도 링크가 걸릴 수 있게 로직 수정
    
    * fix: 기존 develop branch와 fetch되지 않은 내용 수정

--- front/src/components/SpeechBubble/SpeechBubble.js
+++ front/src/components/SpeechBubble/SpeechBubble.js
@@ -1,9 +1,26 @@
 import './SpeechBubble.scss';
 
+import { PATTERNS } from '../../constants/regex';
 import PropTypes from 'prop-types';
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
 
 const SpeechBubble = ({ time, type = 'mine', children }) => {
+  const linkReg = new RegExp(PATTERNS.LINKS);
+  const linkArray = children.match(linkReg);
+  const chat = { children: children };
+
+  if (linkArray) {
+    const innerElement = linkArray.map((link) =>
+      children.replace(
+        link,
+        `<a href="${link}" target='_blank' rel='noopener noreferrer'>${link}</a>`
+      )
+    );
+
+    chat.children = innerElement.join('');
+  }
+
   return (
     <div
       className={`speech-bubble-container ${type}-container ${
@@ -11,7 +28,7 @@ const SpeechBubble = ({ time, type = 'mine', children }) => {
       }`}
     >
       <div className={`speech-bubble ${type}`}>
-        <pre className='text'>{children}</pre>
+        <pre className='text'>{ReactHtmlParser(chat.children)}</pre>
         <time className='time'>{time}</time>
       </div>
     </div>
