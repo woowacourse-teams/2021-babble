1718c7abf497ba53ad203073456ae18af01eafc5
ROOT <junroot0909@gmail.com>
2021-10-22 22:51:41 +0900

    refactor: 불필요한 임포트 제거

--- back/babble/src/main/java/gg/babble/babble/domain/repository/TagRepository.java
+++ back/babble/src/main/java/gg/babble/babble/domain/repository/TagRepository.java
@@ -1,10 +1,8 @@
 package gg.babble.babble.domain.repository;
 
 import gg.babble.babble.domain.tag.Tag;
-import gg.babble.babble.dto.response.TagNameResponse;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -21,5 +19,5 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
         + "    left join alternative_tag_name on alternative_tag_name.deleted = false and alternative_tag_name.tag_id = tag.id\n"
         + "where tag.name like %:keyword%\n"
         + "    or alternative_tag_name.name like %:keyword%", nativeQuery = true)
-    List<Tag> findAllByKeyword(@Param("keyword")  final String keyword, final Pageable page);
+    List<Tag> findAllByKeyword(@Param("keyword") final String keyword, final Pageable page);
 }
