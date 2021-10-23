20bc3fd61f4410913b8663af2627c536f3d2861e
lns13301 <rkdrbgks22@ks.ac.kr>
2021-10-24 02:15:40 +0900

    refactor: String Dto 검증 애너테이션 변경

--- back/babble/src/main/java/gg/babble/babble/dto/request/post/PostCreateRequest.java
+++ back/babble/src/main/java/gg/babble/babble/dto/request/post/PostCreateRequest.java
@@ -4,6 +4,7 @@ import static gg.babble.babble.domain.post.Account.MAX_PASSWORD_LENGTH;
 import static gg.babble.babble.domain.post.Account.MIN_PASSWORD_LENGTH;
 
 import gg.babble.babble.domain.post.Post;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -17,16 +18,16 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PostCreateRequest {
 
-    @NotNull(message = "게시글 제목은 공백일 수 없습니다.")
+    @NotEmpty(message = "게시글 제목은 공백일 수 없습니다.")
     private String title;
 
-    @NotNull(message = "내용이 작성되지 않았습니다.")
+    @NotEmpty(message = "내용이 작성되지 않았습니다.")
     private String content;
 
-    @NotNull(message = "카테고리가 선택되지 않았습니다.")
+    @NotEmpty(message = "카테고리가 선택되지 않았습니다.")
     private String category;
 
-    @NotNull(message = "닉네임은 공백일 수 없습니다.")
+    @NotEmpty(message = "닉네임은 공백일 수 없습니다.")
     private String nickname;
 
     @Size(min = MIN_PASSWORD_LENGTH, max = MAX_PASSWORD_LENGTH)
--- back/babble/src/main/java/gg/babble/babble/dto/request/post/PostUpdateRequest.java
+++ back/babble/src/main/java/gg/babble/babble/dto/request/post/PostUpdateRequest.java
@@ -3,6 +3,7 @@ package gg.babble.babble.dto.request.post;
 import static gg.babble.babble.domain.post.Account.MAX_PASSWORD_LENGTH;
 import static gg.babble.babble.domain.post.Account.MIN_PASSWORD_LENGTH;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -16,18 +17,19 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PostUpdateRequest {
 
-    @NotNull(message = "게시글 id 가 비어있습니다.")
+    @NotEmpty(message = "게시글 id 가 비어있습니다.")
     private Long id;
 
-    @NotNull(message = "게시글 제목은 공백일 수 없습니다.")
+    @NotEmpty(message = "게시글 제목은 공백일 수 없습니다.")
     private String title;
 
-    @NotNull(message = "내용이 작성되지 않았습니다.")
+    @NotEmpty(message = "내용이 작성되지 않았습니다.")
     private String content;
 
-    @NotNull(message = "카테고리가 선택되지 않았습니다.")
+    @NotEmpty(message = "카테고리가 선택되지 않았습니다.")
     private String category;
 
+    @NotEmpty(message = "해당 게시글의 비밀번호가 입력되지 않았습니다.")
     @Size(min = MIN_PASSWORD_LENGTH, max = MAX_PASSWORD_LENGTH)
     private String password;
 }
