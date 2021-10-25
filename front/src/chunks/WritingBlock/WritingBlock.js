9fb96170e27fb5c804f3d115428dee1fb8057a87
iborymagic <mlch8023@gmail.com>
2021-10-26 03:46:49 +0900

    refactor: WritingBlock에서 로직을 제거 - WritePost로 이동

--- front/src/chunks/WritingBlock/WritingBlock.js
+++ front/src/chunks/WritingBlock/WritingBlock.js
@@ -1,21 +1,16 @@
 import './WritingBlock.scss';
 import '../../../node_modules/quill/dist/quill.snow.css';
 
-import { Body2, Subtitle1 } from '../../core/Typography';
-import { DropdownInput, SquareButton, TextInput } from '../../components';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import PropTypes from 'prop-types';
 import Quill from 'quill';
+import { TextInput } from '../../components';
 
-const WritingBlock = ({ submitPost }) => {
-  const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('');
+const WritingBlock = () => {
   const editorRef = useRef(null);
 
   useEffect(() => {
-    setCategories(['자유', '건의', '게임', '공지']);
-
     editorRef.current = new Quill('.editor', {
       modules: {
         toolbar: [
@@ -33,83 +28,36 @@ const WritingBlock = ({ submitPost }) => {
     });
   }, []);
 
-  const handleSubmitButtonClick = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget.form;
-    const title = form.title.value;
-    const content = form.querySelector('.ql-editor').innerHTML;
-    const nickname = form.nickname.value;
-    const password = form.password.value;
-
-    const post = {
-      title,
-      content,
-      nickname,
-      password,
-      category: selectedCategory || '자유',
-    };
-
-    submitPost(post);
-  };
-
   return (
-    <section className='writing-section'>
-      <div className='writing-header'>
-        <Subtitle1>게시글 작성</Subtitle1>
-        <div className='category'>
-          <DropdownInput
-            type='text'
-            placeholder='카테고리를 선택하세요.'
-            dropdownKeywords={categories}
-            inputValue={selectedCategory}
-            setInputValue={setSelectedCategory}
-          />
-        </div>
+    <div className='writing-block'>
+      <div className='title'>
+        <TextInput
+          name='title'
+          border={false}
+          placeholder='제목을 입력하세요.'
+          required
+        />
       </div>
-      <form className='writing-form'>
-        <div className='writing-block'>
-          <div className='title'>
-            <TextInput
-              name='title'
-              border={false}
-              placeholder='제목을 입력하세요.'
-              required
-            />
-          </div>
-          <div className='editor-container'>
-            <div className='editor'></div>
-          </div>
-          <div className='writing-info'>
-            <TextInput
-              name='nickname'
-              border={false}
-              placeholder='닉네임'
-              required
-            />
-            <TextInput
-              name='password'
-              minLength={4}
-              border={false}
-              placeholder='비밀번호(4자 이상)'
-              required
-            />
-          </div>
-        </div>
-        <SquareButton
-          type='submit'
-          size='block'
-          name='write'
-          onClickButton={handleSubmitButtonClick}
-        >
-          <Body2>작성하기</Body2>
-        </SquareButton>
-      </form>
-    </section>
+      <div className='editor-container'>
+        <div className='editor'></div>
+      </div>
+      <div className='writing-info'>
+        <TextInput
+          name='nickname'
+          border={false}
+          placeholder='닉네임'
+          required
+        />
+        <TextInput
+          name='password'
+          minLength={4}
+          border={false}
+          placeholder='비밀번호(4자 이상)'
+          required
+        />
+      </div>
+    </div>
   );
-};
-
-WritingBlock.propTypes = {
-  submitPost: PropTypes.func,
 };
 
 export default WritingBlock;
--- front/src/pages/WritePost/WritePost.js
+++ front/src/pages/WritePost/WritePost.js
@@ -1,13 +1,18 @@
 import './WritePost.scss';
 
-import { MainImage, ModalError } from '../../components';
+import { Body2, Headline2, Subtitle1 } from '../../core/Typography';
+import {
+  DropdownInput,
+  MainImage,
+  ModalError,
+  SquareButton,
+} from '../../components';
+import React, { useEffect, useState } from 'react';
 
 import { BASE_URL } from '../../constants/api';
-import { Headline2 } from '../../core/Typography';
 import { Link } from 'react-router-dom';
 import PATH from '../../constants/path';
 import PageLayout from '../../core/Layout/PageLayout';
-import React from 'react';
 import { WritingBlock } from '../../chunks';
 import axios from 'axios';
 import { useDefaultModal } from '../../contexts/DefaultModalProvider';
@@ -17,9 +22,28 @@ const WritePost = () => {
   const history = useHistory();
   const { openModal } = useDefaultModal();
 
-  const submitPost = async (postObj) => {
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const submitPost = async (e) => {
+    e.preventDefault();
+
+    const form = e.currentTarget.form;
+    const title = form.title.value;
+    const content = form.querySelector('.ql-editor').innerHTML;
+    const nickname = form.nickname.value;
+    const password = form.password.value;
+
+    const post = {
+      title,
+      content,
+      nickname,
+      password,
+      category: selectedCategory || '자유',
+    };
+
     try {
-      const response = await axios.post(`${BASE_URL}/api/post`, postObj);
+      const response = await axios.post(`${BASE_URL}/api/post`, post);
       const postData = response.data;
 
       history.push(`${PATH.BOARD}${PATH.VIEW_POST}/${postData.id}`);
@@ -27,6 +51,10 @@ const WritePost = () => {
       openModal(<ModalError>글 작성에 실패했습니다.</ModalError>);
     }
   };
+
+  useEffect(() => {
+    setCategories(['자유', '건의', '게임', '공지']);
+  }, []);
 
   return (
     <main className='board-container'>
@@ -37,7 +65,31 @@ const WritePost = () => {
             <Headline2>익명 게시판</Headline2>
           </Link>
         </div>
-        <WritingBlock submitPost={submitPost} />
+        <section className='writing-section'>
+          <div className='writing-header'>
+            <Subtitle1>게시글 작성</Subtitle1>
+            <div className='category'>
+              <DropdownInput
+                type='text'
+                placeholder='카테고리를 선택하세요.'
+                dropdownKeywords={categories}
+                inputValue={selectedCategory}
+                setInputValue={setSelectedCategory}
+              />
+            </div>
+          </div>
+          <form className='writing-form'>
+            <WritingBlock />
+            <SquareButton
+              type='submit'
+              size='block'
+              name='write'
+              onClickButton={submitPost}
+            >
+              <Body2>작성하기</Body2>
+            </SquareButton>
+          </form>
+        </section>
       </PageLayout>
     </main>
   );
