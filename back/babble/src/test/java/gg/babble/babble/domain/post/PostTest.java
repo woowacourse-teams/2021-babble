5e37e3702e75aeb48390d6bb8e35e3d696f6a7ab
ROOT <junroot0909@gmail.com>
2021-10-27 04:15:36 +0900

    test: 잘못된 테스트 수정

--- back/babble/src/test/java/gg/babble/babble/domain/post/PostTest.java
+++ back/babble/src/test/java/gg/babble/babble/domain/post/PostTest.java
@@ -2,6 +2,7 @@ package gg.babble.babble.domain.post;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import gg.babble.babble.exception.BabbleIllegalArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,11 +14,12 @@ class PostTest {
         String sentence = "a";
         StringBuilder content = new StringBuilder();
 
-        while (content.toString().getBytes().length < 8000) {
+        while (content.toString().getBytes().length <= 8000) {
             content.append(sentence);
         }
 
-        assertThatThrownBy(() -> new Post("title", content.toString(), "category", "nickname", "password"));
+        assertThatThrownBy(() -> new Post("title", content.toString(), "자유", "nickname", "password"))
+            .isExactlyInstanceOf(BabbleIllegalArgumentException.class);
     }
 
     @DisplayName("게시글 제목이 50글자보다 크면 예외 처리")
@@ -30,6 +32,7 @@ class PostTest {
             title.append(sentence);
         }
 
-        assertThatThrownBy(() -> new Post(title.toString(), "content", "category", "nickname", "password"));
+        assertThatThrownBy(() -> new Post(title.toString(), "content", "자유", "nickname", "password"))
+            .isExactlyInstanceOf(BabbleIllegalArgumentException.class);;
     }
 }
