133bd4bd5f7f2fde0860fb48925f668e5278de13
Min Kyung Kang <mnk918p@gmail.com>
2021-10-27 22:46:52 +0900

    fix: submit error 해결

--- front/src/components/ChattingForm/ChattingForm.js
+++ front/src/components/ChattingForm/ChattingForm.js
@@ -58,7 +58,6 @@ const ChattingForm = ({ onSubmit }) => {
         onChange={blockWhenMaxLength}
         autoFocus
       ></textarea>
-
       {overMaxLength && (
         <span className='alert-container'>
           <img src='https://babble.gg/img/icons/warning.png' alt='warning' />
