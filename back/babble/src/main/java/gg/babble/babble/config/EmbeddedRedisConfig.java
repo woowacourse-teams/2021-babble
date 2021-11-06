7c2f01639a2561c248300955bb4b79c1e647d987
lns13301 <rkdrbgks22@ks.ac.kr>
2021-11-06 11:38:40 +0900

    refactor: EmbeddedRedisConfig profile 변경

--- back/babble/src/main/java/gg/babble/babble/config/EmbeddedRedisConfig.java
+++ back/babble/src/main/java/gg/babble/babble/config/EmbeddedRedisConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
-@Profile("locala")
+@Profile("local")
 @Configuration
 public class EmbeddedRedisConfig {
 
